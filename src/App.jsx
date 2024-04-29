d5cea1ad0cae9a9e3d3327513506c12c9d1ac2fb
kimYejin <kimphoby0125@gmail.com>
2024-04-29 11:28:27 +0900

    pops

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hello from './Hello';
+import './App.css';
+import Button from './components/Button'
+
+function App() {
+    return (
+      <>
+      <Button text={"메일"} color={"red"}/>
+      <Button text={"카페"}/>
+      <Button text = {"블로그"}/>
+      </>
+  );
+}
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+const Button = (props) => {
+    console.log(props);
+    return (
+    <button style={{color:props.color}}>{props.text}</button>
+    );
+};
+
+export default Button;
